6779ca1226eb6df933c0900156d18e424e016088
614259037 <61488264+614259037@users.noreply.github.com>
2021-03-16 20:28:36 +0700

    Update Searchpage.js

--- src/page/Searchpage.js
+++ src/page/Searchpage.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Search from "../components/Search"
 const Searchpage = () => {
     return (
-        <>
+        <div>
             <Search />
-        </>
+        </div>
     )
 }
 
